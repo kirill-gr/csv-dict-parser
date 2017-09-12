271072d7b8df3ed7fb3b748c6d7cd6972535aeff
Krom <krommail@gmail.com>
2017-09-13 00:29:18 +0300

    Parameters simplified, input and output files management added

--- src/main/kotlin/ru/grushetsky/app.kt
+++ src/main/kotlin/ru/grushetsky/app.kt
@@ -1,35 +1,49 @@
 package ru.grushetsky
 
+import java.io.File
+
 fun main(args: Array<String>) {
-    // TODO parse input params
-    val (fileName, outputPath) = parseArgs(args)
-    // TODO check file exists
-    checkExists(fileName)
-    // TODO create output file
-    createOutputFile()
-    // TODO read file and open output file
-    // TODO parse row
+    val fileName = parseArgs(args)
+    val inputFile = getInputFile(fileName)
+    val outputFile = getOutputFile(fileName)
     parse()
-    // TODO save parsed data to an output file
     saveOutputFile()
 }
 
-fun parseArgs(args: Array<String>): Pair<String, String> {
-    return Pair(args.first(), args[1])
+fun parseArgs(args: Array<String>): String {
+    when (args.size) {
+        1 -> return args.first()
+        else -> throw IllegalArgumentException("Too many arguments")
+    }
 }
 
 fun saveOutputFile() {
+    // TODO save parsed data to an output file
     TODO("not implemented")
 }
 
 fun parse() {
+    // TODO read file and open output file
+    // TODO parse row
     TODO("not implemented")
 }
 
-fun createOutputFile() {
-    TODO("not implemented")
+fun getInputFile(fileName: String): File {
+    val file = File(fileName)
+    if (!file.canRead()) {
+        throw IllegalArgumentException("File $fileName doesn't exist or is not accessible")
+    }
+    return file
 }
 
-fun checkExists(fileName: String): Boolean {
-    TODO("not implemented")
+fun getOutputFile(fileName: String): File {
+    val outputFileName = fileName + ".json"
+    val file = File(outputFileName)
+    if (file.exists()) {
+        throw IllegalArgumentException("File $outputFileName already exists, delete it first")
+    }
+    if (!file.canWrite()) {
+        throw IllegalArgumentException("File $outputFileName is not writable")
+    }
+    return file
 }
--- src/test/kotlin/ru/grushetsky/AppTest.kt
+++ src/test/kotlin/ru/grushetsky/AppTest.kt
@@ -1,18 +1,27 @@
 package ru.grushetsky
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 
 internal class AppTest {
     @Test
     fun `should return two strings when array of two strings passed`() {
         val fileName = "file"
-        val dirName = "dir"
-        val args = arrayOf(fileName, dirName)
+        val args = arrayOf(fileName)
 
-        val (fileNameActual, dirNameActual) = parseArgs(args)
+        val fileNameActual = parseArgs(args)
 
         assertEquals(fileName, fileNameActual)
-        assertEquals(dirName, dirNameActual)
     }
+
+    @Test
+    fun `should throw if file doesn't exist`() {
+        assertThrows(
+                IllegalArgumentException::class.java,
+                {getInputFile("test")},
+                "File test doesn't exist"
+        )
+    }
+
 }
